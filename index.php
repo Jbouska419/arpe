e020720c14fe5446396713eafbcf3d59ac6350c9
Jbouska419 <Jbouska419@gmail.com>
2013-08-13 14:51:20 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Arpe Constructions</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.20" />
+	<link rel="stylesheet" type="text/css" href="includes/style.css">
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+	<script type="text/javascript" src="galleria/galleria-1.2.7.min.js"></script>
+	<script src="_js/jquery-1.6.3.min.js"></script>
+<script src="_js/nav1.1.min.js"></script>
+<script>
+$(document).ready(function() {
+ $("#navigation").navPlugin({
+   'itemWidth': 189.6,
+   'itemHeight': 26,
+   'navEffect': "slide",
+   'speed': 250
+ });
+}); // end ready
+</script>
+</head>
+
+<body>
+	<div class="shell">
+		<div id="header">
+			<div id="logo">
+			<div id="logo2">
+			</div> <!-- logo2-->
+			</div> <!--logo-->
+		</div> <!--header-->
+		
+		<div id = "nav"> 
+			<ul id="navigation" >
+        <li><a href="index.php">Home</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="gallery.php">Photo Gallery </a>
+          <ul>
+            <li><a href="gallery.php">Interior Services</a>
+              <ul>
+                <li><a href="kitchens.php">Kitchens</a></li>
+                <li><a href="bathrooms.php">Bathrooms</a></li>
+                <li><a href="basements.php">Basements</a></li>
+                <li><a href="flooring.php">Hardwood Flooring</a></li>
+              </ul>
+            </li>
+            <li><a href="gallery.php">Exterior Services</a>
+              <ul>
+                <li><a href="decks.php">Decks</a></li>
+                <li><a href="gazebos.php">Gazebos</a></li>
+                <li><a href="porches.php">Front Porches</a>
+                <li><a href="pergolas.php">Pergolas</a>
+                <li><a href="pools.php">Pool &amp; Hot Tub Decks</a>
+                <li><a href="stairs.php">Staircases</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+			<li><a href="testimonials.php">Testimonials</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+      </ul>
+		</div><!-- nav -->
+		
+		<div id="content">
+			<div class="cl20"></div>
+			<div class="l-content-box">
+				<h2>What our clients are saying...</h2>
+					<p>"Simply amazing work.  Great job guys!!!"</br>
+					<span>-Jim B. -Warrenville, IL</span></p>
+					<p>"Carlos is THE man...very knowledgeable..."</br>
+					<span>-Carlos Tisoc -Westmont, IL</span></p>
+					<p>"Two words..."THE BEST"...all of the work was professionally done and completed on time..."</br>
+					<span>-Laura Sosnowski - The Clubhouse</span></p>
+			</div> <!-- l-content-box -->
+			
+			<div class="r-content">
+				<p>&nbsp;&nbsp;&nbsp;With more than 10 years of experience, ARPE Constructions is dedicated to bring you the best quality in
+				home projects at an affordable price.  We specialize in exterior projects such as decks, fences, gazebos,
+				pergolas, screened porches, and four season rooms.  We are also qualified in interior projects as well, everything
+				from kitchen remodeling, bathrooms, and finishing basements.  We can complete all of your construction needs.</p>
+				
+				<p>&nbsp;&nbsp;&nbsp;Our goal is 100% satisfaction of our clients from the initial consultation to the completion of your
+				construction project.  We will bring your ideas to life, or we can build your project from and existing
+				blueprint. We will meet any budget!</p>
+			</div> <!-- r-content -->
+			<div class="cl">
+			</div> <!--cl-->
+		</div> <!-- content -->
+		
+		<div id="footer">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="services.php">Services</a></li>
+				<li><a href="gallery.php">Photo Gallery</a></li>
+				<li><a href="testimonials.php">Testimonials</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+			
+			<div class="right">
+				<span>&copy; 2012 - ARPE Constructions &nbsp; Design by James Bouska</span>
+			</div> <!-- right -->
+			
+		</div> <!-- footer -->
+			
+			
+	</div> <!-- shell -->
+	
+</body>
+
+</html>
