9523101b364c35c0fc5618be9ec8652df970b100
Jbouska419 <Jbouska419@gmail.com>
2013-08-13 14:52:20 -0500

    Create services.php

---
+++ services.php
@@ -0,0 +1,38 @@
+<?php # services.php
+$page_title = 'Services';
+include ('includes/header.php');
+?>
+	
+	<div id = "content">
+		<div class="cl20"></div>
+		<h3>Take a look at what Arpe Constructions can do for you.</h3>
+		
+		<div class = "left-column">
+			<h4>Interior Services</h4>
+				<ul>
+					<li><a href = "kitchens.php">Kitchen Remodeling</a></li>
+					<li><a href = "bathrooms.php">Bathroom Remodeling</a></li>
+					<li><a href = "basements.php">Basement Finishing</a></li>
+					<li><a href = "hardwood.php">Hardwood Flooring</a></li>
+				</ul>
+		</div> <!-- left-column -->
+		
+		<div class = "right-column">
+			<h4>Exterior Services</h4>
+				<ul>
+					<li><a href = "decks.php">Decks</a></li>
+					<li><a href = "gazebos.php">Gazebos</a></li>
+					<li><a href = "porches.php">Front Porches</a></li>
+					<li><a href = "pergolas.php">Pergolas</a></li>
+					<li><a href = "pools.php">Pool and Hot Tub Decks</a></li>
+					<li><a href = "stairs.php">Staircases</a></li>
+				</ul>
+		</div> <!-- right-column -->
+		<div class = "cl"></div>
+	</div> <!-- content -->
+	
+	<div class = "cl"></div>
+	
+	<?php
+	include ('includes/footer.html');
+	?>
